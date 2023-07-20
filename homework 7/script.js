421e6e2af563317fcc14100cf6fa21582a015c9f
Bautik55 <138191321+Bautik55@users.noreply.github.com>
2023-07-20 11:58:52 +0900

    Add files via upload

---
+++ homework 7/script.js
@@ -0,0 +1,34 @@
+// const array  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+// const foro = (array) => {
+//     let sum = 0
+//     for (let index  = 0 ; index < array.length; index++ ) {
+//         sum += array [index]
+//     }
+//     return sum;
+// }
+//
+// console.log(foro(array))
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let result = 0;
+//
+// for (let i = 0; i < array.length; i++) {
+//     result += array[i];
+// }
+//
+// console.log(result);
+
+// const array = [1, 2, 5, 9, 13, 10];
+// let found = false;
+//
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 4) {
+//         found = true;
+//         break;
+//     }
+// }
+//
+// if (found) {
+//     console.log('Есть');
+// }
